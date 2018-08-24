5bdfe00638453cc28bc5bf66cf4163d5a8e54a1a
Your Name <you@example.com>
2018-08-25 05:34:36 +0900

    青側用のランダムウォーク追加、赤側ウェイポイント追加

--- scripts/start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# set judge server state "running"
-bash onigiri_war_judge/test_scripts/set_running.sh localhost:5000
-
-# launch robot control node
-roslaunch onigiri_war sim_robot_run.launch
